81c55df1c75537069e219f5d85c17971a14ea621
Colin Tang <ct60@rice.edu>
2025-04-08 15:18:21 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser'); 
+const auth = require('./auth.js');
+const cors = require('cors');
+const articles = require('./articles.js');
+const profile = require('./profile.js');
+
+ const hello = (req, res) => res.send({ hello: 'world' });
+
+const app = express();
+const allowedOrigins = [
+     'https://owl-connect.surge.sh',
+     'http://localhost:3000'
+   ];
+const corsOptions = {origin: allowedOrigins, credentials: true};
+app.use(cors(corsOptions));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.set('trust proxy', 1);
+app.get('/', hello);
+app.set('userObjs', {});
+auth(app);
+articles(app);
+profile(app);
+
+
+// Get the port from the environment, i.e., Heroku sets it
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+     const addr = server.address();
+     console.log(`Server listening at http://${addr.address}:${addr.port}`)
+});
